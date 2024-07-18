aab2dc15498a5b4f6eb4bf3497af56fadb2f292d
Patriott07 <pwangtampn@gmail.com>
2024-07-18 19:59:07 +0700

    commit faiz

--- client/src/pages/Menus/Recommendation.jsx
+++ client/src/pages/Menus/Recommendation.jsx
@@ -12,8 +12,10 @@ function Recommendation() {
             <h3 class="font-semibold text-base text-blueGray-700">Rekomendasi</h3>
           </div>
           <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+          {currentUser.role != 'user' ? (
             <button class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Buat rekomendasi aktivitas</button>
-          </div>
+          ) : null }
+            </div>
         </div>
       </div>
 
--- client/src/pages/Menus/Treatment.jsx
+++ client/src/pages/Menus/Treatment.jsx
@@ -12,8 +12,10 @@ function Treatment() {
             <h3 class="font-semibold text-base text-blueGray-700">Treatment</h3>
           </div>
           <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+          {currentUser.role != 'user' ? (
             <button class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Buat Treatment</button>
-          </div>
+          ) : null }
+            </div>
         </div>
       </div>
 
