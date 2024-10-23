e45be8e3faa9b7864150d80db6119037de74a947
m-faiz07 <muhammad.faiz44367@smk.belajar.id>
2024-10-23 07:25:27 +0700

    23-10-2024 Faiz, membuat API mengambil dari file

--- api/routes/data.route.js
+++ api/routes/data.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getFilteredAndRawData } from '../controllers/user.controller.js';
+import { getFilteredAndRawData, fetchDailyData } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 // Rute untuk mendapatkan data yang sudah difilter dan data mentah
 router.get('/filtered-raw', getFilteredAndRawData);
-
+router.get('/daily-data', fetchDailyData);
 export default router;
